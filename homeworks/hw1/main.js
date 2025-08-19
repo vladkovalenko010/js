007ca0b4a84e107f914635b3f6be52c602873aec
vladkovalenko010 <vladkovalenko010@gmail.com>
2025-08-19 10:43:56 +0200

    hw1 resolved

---
+++ homeworks/hw1/main.js
@@ -0,0 +1,45 @@
+// #dYQNrBV
+
+let s1 = 'hello';
+let s2 = 'owu';
+let s3 = 'com';
+let s4 = 'ua';
+let n1 = 1;
+let n2 = 10;
+let n3 = -999;
+let n4 = 123;
+let n5 = 3.14;
+let n6 = 2.7;
+let n7 = 16;
+let b1 = true;
+let b2 = false;
+
+console.log(s1);
+console.log(s2);
+console.log(s3);
+console.log(s4);
+console.log(n1);
+console.log(n2);
+console.log(n3);
+console.log(n4);
+console.log(n5);
+console.log(n6);
+console.log(n7);
+console.log(b1);
+console.log(b2);
+
+// #6Qb97gsv
+
+let firstName = 'Vladyslav';
+let middleName = 'Serhiiyovych';
+let lastName = 'Kovalenko';
+
+let person = `${firstName} ${middleName} ${lastName}`;
+
+// #4N0y5tufA
+
+let a = 100;
+let b = '100';
+let c = true;
+
+console.log(typeof a, a, typeof b, b, typeof c, c);
