c83b4f70f7553ec65a506deba2bddf0741b68d2a
vladkovalenko010 <vladkovalenko010@gmail.com>
2025-08-22 10:52:30 +0200

    cw3 resolved

---
+++ classworks/3/1 for of/main.js
@@ -0,0 +1,14 @@
+let coursesAndDurationArray = [
+    {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4}
+];
+
+for (let course of coursesAndDurationArray) {
+    if (course.monthDuration > 5) {
+        console.log(course);
+    }
+}
